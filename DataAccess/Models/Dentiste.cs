73f06f4b2abc31d1c1d28d06b4e16197258d1b3e
yassine ktiri <yassinektiri@gmail.com>
2022-04-26 00:48:48 +0000

    final commit

---
+++ DataAccess/Models/Dentiste.cs
@@ -0,0 +1,13 @@
+﻿namespace DataAccess.Models
+{
+    public class Dentiste
+    {
+        public Guid Dentiste_id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public int Debut_travail { get; set; }
+        public int Fin_travail { get; set; }
+        public int Max_clients { get; set; }
+
+    }
+}
---
+++ DataAccess/Models/RendezVous.cs
@@ -0,0 +1,14 @@
+﻿namespace DataAccess.Models
+{
+    public class RendezVous
+    {
+        public Guid Rdv_id { get; set; }
+        public Guid Dentiste_id { get; set; }
+        public Guid  Client_id { get; set; }
+        public Guid  Consultation_id { get; set; }
+        public DateTime Date_rdv { get; set; }
+        public bool Annule { get; set; } = false;
+        public string Reason { get; set; } = "";
+        public bool Paye { get; set; } = false;
+    }
+}
